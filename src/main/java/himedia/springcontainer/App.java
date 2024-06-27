547936559cf5fa3199c32699dc5b2cecdd7dfb98
Blt24Q1 <blt24q1@gmail.com>
2024-06-27 12:24:14 +0900

    Inject

--- src/main/java/himedia/springcontainer/App.java
+++ src/main/java/himedia/springcontainer/App.java
@@ -23,5 +23,24 @@ public class App {
 		User user3 = (User)ac.getBean("user3");
 		
 		System.out.println(user1 == user3);
+		
+		//	생성자 매개변수 전달
+		User user4 = (User)ac.getBean("user4");
+		System.out.println("user4:" + user4);
+		
+		//	Setter를 이용한 필드 설정
+		User user5 = (User)ac.getBean("user5");
+		System.out.println("user5:" + user5);
+		
+		//	참조 주입
+		Friend friend6 = (Friend)ac.getBean("friend6"); //	주입될 객체
+		System.out.println("friend6:" + friend6);
+		
+		User user6 = (User)ac.getBean("user6");
+		System.out.println("user6:" + user6);
+		
+		//	집합 객체 주입
+		User user7 = (User)ac.getBean("user7");
+		System.out.println("user7:" + user7);
 	}
 }
